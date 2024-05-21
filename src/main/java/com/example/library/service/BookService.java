bc8e5c3beae9820d3b6326c8068012e1c5a32123
kos-nastiia <kostyrka.anastasiia@gmail.com>
2024-05-21 21:54:34 +0300

    yuiop

--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -2,6 +2,7 @@ package com.example.library.service;
 
 import java.util.List;
 import com.example.library.model.Book;
+
 public interface BookService {
     public Book saveBook(Book book);
     public List<Book> getAllBooks();
